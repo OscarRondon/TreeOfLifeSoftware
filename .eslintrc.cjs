1c7749f0f5c53c55f979e864920253eceabc6d79
orondon <oscar.rondon.c@gmail.com>
2023-05-14 13:20:51 -0400

    Project Initialization

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    './node_modules/ts-standard/eslintrc.json'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/method-signature-style': 'off',
+    'no-multiple-empty-lines': 'off',
+    'padded-blocks': 'off'
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is the Company page for Tree of Life Software
