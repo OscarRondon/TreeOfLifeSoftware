759eba29517e09b92c478b6c53fa3f63a56d2e87
orondon <oscar.rondon.c@gmail.com>
2023-05-23 11:29:15 -0400

    Footer fix sticky, Add Main section styles

--- src/components/AppMain.tsx
+++ src/components/AppMain.tsx
@@ -1,7 +1,12 @@
+import { Home } from './Home'
+
 export function AppMain () {
   return (
-    <main className='border'>
-      <h1>Main component!!</h1>
+    <main className='main border'>
+      <Home />
+      <Home />
+      <Home />
+      <Home />
     </main>
   )
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+export function Home () {
+  return (
+    <section className='container page-width page-height border'>
+      <h1 className='title-h1'>Tree of Life Software</h1>
+    </section>
+  )
+}
