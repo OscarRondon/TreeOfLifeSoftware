8e46a000ad522cdb5fa219220b9814b85e5766d2
orondon <oscar.rondon.c@gmail.com>
2023-05-24 17:25:33 -0400

    Creating home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,11 @@
 export function Home () {
   return (
     <section className='container page-width page-height border'>
+      <img src='/src/assets/Images/images/SoftwareDevelopment.png' />
       <h1 className='title-h1'>Tree of Life Software</h1>
+      <p>As professionals on technologies and business, we are here to help you succeed. We are a team of passionate software developers and designers who are committed to helping you succeed. we understand that your business is constantly changing and evolving. We will be there with you every step of the way providing the resources and support you need to succeed.</p>
+      <p>We are a US based software development consultant firm with over +15 years of custom software experience in design, development and software implementation</p>
+      <p>Trusted guidance, global expertise, secure integration. We design and develop custom software solutions that deliver digital transformation at scale.</p>
     </section>
   )
 }
