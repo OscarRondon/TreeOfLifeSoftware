53c1252659d2e00c7c4aa09a9a5cf26735b94fac
orondon <oscar.rondon.c@gmail.com>
2023-05-25 18:52:42 -0400

    Home done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,17 @@
 export function Home () {
   return (
     <section className='container page-width page-height border'>
-      <img src='/src/assets/Images/images/SoftwareDevelopment.png' />
-      <h1 className='title-h1'>Tree of Life Software</h1>
-      <p>As professionals on technologies and business, we are here to help you succeed. We are a team of passionate software developers and designers who are committed to helping you succeed. we understand that your business is constantly changing and evolving. We will be there with you every step of the way providing the resources and support you need to succeed.</p>
-      <p>We are a US based software development consultant firm with over +15 years of custom software experience in design, development and software implementation</p>
-      <p>Trusted guidance, global expertise, secure integration. We design and develop custom software solutions that deliver digital transformation at scale.</p>
+      <div className='home-container'>
+        <img src='/src/assets/Images/images/SoftwareDevelopment.png' />
+        <div className='home-text'>
+          <h1 className='title-h1'><strong>Tree of Life Software</strong></h1>
+          <p>As professionals on technologies and business, we are here to help you succeed.</p>
+          <p>We understand that your business is constantly changing and evolving, looking for the competitive advantage that make you unique.</p>
+          <p> We will be there with you every step of the way providing the resources and support you need toreach your goals.</p>
+          <p>We are a US based software development consultant firm with over <span className='number'>+15</span> years of custom software experience in design, development and software implementation</p>
+          <p>We design and develop custom software solutions that deliver digital transformation and improve the lives of our customers.</p>
+        </div>
+      </div>
     </section>
   )
 }
