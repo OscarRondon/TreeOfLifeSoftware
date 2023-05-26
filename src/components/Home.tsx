80653c79878c05c5fbe0d6ded6af2a5cf45e9e73
orondon <oscar.rondon.c@gmail.com>
2023-05-26 11:33:22 -0400

    Home responsive done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,17 @@
 export function Home () {
   return (
-    <section className='container page-width page-height border'>
+    <section className='container page-width page-height'>
       <div className='home-container'>
         <img src='/src/assets/Images/images/SoftwareDevelopment.png' />
         <div className='home-text'>
-          <h1 className='title-h1'><strong>Tree of Life Software</strong></h1>
+          <h1 className='home-title-h1'><strong>Tree of Life Software</strong></h1>
           <p>As professionals on technologies and business, we are here to help you succeed.</p>
           <p>We understand that your business is constantly changing and evolving, looking for the competitive advantage that make you unique.</p>
-          <p> We will be there with you every step of the way providing the resources and support you need toreach your goals.</p>
-          <p>We are a US based software development consultant firm with over <span className='number'>+15</span> years of custom software experience in design, development and software implementation</p>
+          <p> We will be there with you every step of the way providing the resources and support you need to reach your goals.</p>
+          <p>We are a US based software development consultant firm with over
+            <span className='number'> +15 </span>
+            years of custom software experience in design, development and software implementation
+          </p>
           <p>We design and develop custom software solutions that deliver digital transformation and improve the lives of our customers.</p>
         </div>
       </div>
