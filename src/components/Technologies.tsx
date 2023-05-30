a0bebebbe1642f45fa82b2b1a8342710a233f344
orondon <oscar.rondon.c@gmail.com>
2023-05-30 17:54:05 -0400

    Add Figure section for technologies

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -8,10 +8,19 @@ export function Technologies () {
   return (
     <section id='technologies' className='container page-width page-height border'>
       <div className='tech-container'>
-        <h2 className='container-title'><strong>Our Technologies Stack</strong></h2>
+        <h2 className='container-title'><strong>Our Technology Stack</strong></h2>
         <div className='tech-list'>
           {
-            techData.map((tech) => { return <p key={tech.id}>Hola</p> })
+            techData.map((tech) => {
+              return (
+                <div key={tech.id}>
+                  <figure>
+                    <img src={tech.imgSrc} alt={tech.name} />
+                    <figcaption>{tech.name}</figcaption>
+                  </figure>
+                </div>
+              )
+            })
           }
         </div>
       </div>
