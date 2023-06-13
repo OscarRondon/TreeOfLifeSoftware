87535b693faf32e2af43376e5ebcae1056b87b68
orondon <oscar.rondon.c@gmail.com>
2023-06-13 17:26:36 -0400

    Tech cards web finished

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -6,7 +6,7 @@ export function Technologies () {
   const techData: Techs[] = technologies as Techs[]
 
   return (
-    <section className='container page-width page-height border'>
+    <section className='container page-width page-height'>
       <div className='tech-container'>
         <h2 className='container-title'>
           <span id='technologies' className='main-inpage-anchor-space' />
@@ -18,8 +18,7 @@ export function Technologies () {
               return (
                 <div key={tech.id} className='tech-card'>
                   <figure>
-                    <img style={{ height: tech.heightvh }} src={tech.imgSrc} alt={tech.name} />
-                    <figcaption>{tech.name}</figcaption>
+                    <img style={{ height: tech.heightvh }} src={tech.imgSrc} alt={tech.name} title={tech.name} />
                   </figure>
                 </div>
               )
