c8ccc02e53b642b991c3adfd29fe319c93385e15
orondon <oscar.rondon.c@gmail.com>
2023-06-30 19:31:07 -0400

    About Us [values list and images]

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -3,35 +3,64 @@ export function AboutUs () {
     <>
       <section className='container page-width page-height'>
         <div className='aboutUs-container'>
-          <div>
-            <span id='aboutUs' className='main-inpage-anchor-space' />
+          <span id='aboutUs' className='main-inpage-anchor-space' />
+          <article>
             <h2>About Us</h2>
             <p>
               Tree of Life Software provides to a small, medium and large companies with efficient, reliable, intuitive and scalable software solutions base and created on best practices software development. We have the knoledge and the experience to deliver a great client exprience.
             </p>
-          </div>
-          <div>
+          </article>
+          <article>
             <img src='/src/assets/Images/images/t1s.png' />
             <h2>Our Mission</h2>
-            <p>Mission Detail</p>
-          </div>
-          <div>
+            <p>Tree of Life Sotware, is focused on using innovative technologies to provide competitive advantages to its clients. Our priority its to make the best software solutions, with the highest standarts that really helps to the efficiency and cost effectiveness of your business.</p>
+          </article>
+          <article>
             <h2>Our Vission</h2>
-            <p>Vission Detail</p>
+            <p>To be a company with an international reputation for technological excellence, quality products, innovative solutions and services. To provide competitive advantages to our customers and become a great place to work so that people feel inspired to give their best.</p>
             <img src='/src/assets/Images/images/t1s.png' />
-          </div>
-          <div>
+          </article>
+          <article>
             <img src='/src/assets/Images/images/t1s.png' />
             <h2>Our Values</h2>
             <ul>
-              <li>Commitment</li>
-              <li>Commitment</li>
-              <li>Commitment</li>
-              <li>Commitment</li>
-              <li>Commitment</li>
-              <li>Commitment</li>
+              <li>
+                <article>
+                  <img src='/src/assets/Images/icons/commitement.png' />
+                  <h3>Commitment</h3>
+                  <p>To create innovative products and offer quality services that generate added value and a positive impact on our customers, we apply to the maximum the capabilities we have developed over the years.</p>
+                </article>
+              </li>
+              <li>
+                <article>
+                  <img src='/src/assets/Images/icons/honesty.png' />
+                  <h3>Honesty</h3>
+                  <p>Behaving with authenticity, making coherent what we think, with what we promise and do. We act with honesty and integrity, respecting ethics and taking responsibility for our actions.</p>
+                </article>
+              </li>
+              <li>
+                <article>
+                  <img src='/src/assets/Images/icons/excellence.png' />
+                  <h3>Excellence</h3>
+                  <p>We are determined to be better people and professionals every day. We focus on generating value to our clients, satisfying their needs and exceeding their expectations thanks to the high standards and continuous improvement applied in all our internal processes.</p>
+                </article>
+              </li>
+              <li>
+                <article>
+                  <img src='/src/assets/Images/icons/passion.png' />
+                  <h3>Passion</h3>
+                  <p>We are motivated by the knowing that we are contributing to the growth of the companies through innovation and digital transformation, improving business viability while creating more and better jobs.</p>
+                </article>
+              </li>
+              <li>
+                <article>
+                  <img src='/src/assets/Images/icons/innovation.svg' />
+                  <h3>Innovation</h3>
+                  <p>We promote new ideas, the continuous improvement of our processes and the creation of solutions that generate high impact in our products and services to satisfy our clients needs . We are diligent, consider the time of others and value the trust they place in us.</p>
+                </article>
+              </li>
             </ul>
-          </div>
+          </article>
         </div>
       </section>
     </>
